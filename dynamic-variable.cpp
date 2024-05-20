2376dd826a532cbe7c4c39dabe2c3146bd87dcf4
checkiamsiam <issiam02415@gmail.com>
2024-05-20 22:13:24 +0600

    chore: Update file associations in VSCode settings.json

---
+++ dynamic-variable.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int *fun()
+{
+    int *a = new int[5]; // dynamic memory won't be deleted after function call
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> a[i];
+    }
+    // delete[] a; // if we delete here then it will give error because we are returning a pointer to a
+    return a;
+}
+int main()
+{
+    // int *p = new int; // dynamic memory will be deleted after function call
+    // *p = 10;
+    // delete p;
+    int *a = fun();
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << " ";
+    }
+    delete[] a;
+    return 0;
+}
