0fd9c863c47ba83f6d599143657bcf3e6a9dc67b
checkiamsiam <issiam02415@gmail.com>
2024-05-27 00:11:28 +0600

    class-object

---
+++ class-constructor.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+  int roll;
+  int cls;
+  double gpa;
+  Student(int roll, int cls, double gpa)
+  {
+    // this keyword is a pointer of current object of the class
+    // (*this).roll = roll;
+    // (*this).cls = cls;
+    // (*this).gpa = gpa;
+    this->roll = roll;
+    this->cls = cls;
+    this->gpa = gpa;
+    }
+};
+int main()
+{
+  Student rahim(29, 9, 5.01);
+  Student karim(45, 10, 4.33);
+  cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+  cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+  return 0;
+}
